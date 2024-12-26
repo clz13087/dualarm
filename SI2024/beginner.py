82c4ab6877bd3819e5a49f3e8a8045e34ebbbabd
clz13087 <clz13087@nitech.jp>
2024-12-26 09:55:43 +0900

    fix

--- SI2024/beginner.py
+++ SI2024/beginner.py
@@ -18,22 +18,22 @@ logging.basicConfig(level=logging.INFO)
 # 動画ファイルのパス
 video_path = glob.glob("/Users/sanolab/this mac/大学/研究室/M2/mastersthesis/data/experiment/expertdata/display/fujiwara/for_master/*expert_video*.mp4")[0]
 
-participant_name = "sugata"
-condition = "imitation"
-# condition = "body integration"
-is_exportdata = False
+participant_name = "obata"
+# condition = "imitation"
+condition = "body integration"
+is_exportdata = True
 
 # before
-# dirPath = f"/Users/sanolab/this mac/大学/研究室/M2/mastersthesis/data/experiment/beginnerdata/display/{condition}/{participant_name}/before"
-# alpha = 0
-# which_camera = 1
-# using_gauge = False
+dirPath = f"/Users/sanolab/this mac/大学/研究室/M2/mastersthesis/data/experiment/beginnerdata/display/{condition}/{participant_name}/before"
+alpha = 0
+which_camera = 2
+using_gauge = False
 
 # practice
-dirPath = f"/Users/sanolab/this mac/大学/研究室/M2/mastersthesis/data/experiment/beginnerdata/display/{condition}/{participant_name}/practice"
-alpha = 0.3
-which_camera = 2
-using_gauge = True
+# dirPath = f"/Users/sanolab/this mac/大学/研究室/M2/mastersthesis/data/experiment/beginnerdata/display/{condition}/{participant_name}/practice"
+# alpha = 0.3
+# which_camera = 2
+# using_gauge = True
 
 # after
 # dirPath = f"/Users/sanolab/this mac/大学/研究室/M2/mastersthesis/data/experiment/beginnerdata/display/{condition}/{participant_name}/after"
